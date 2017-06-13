6b7525fcca3dcbf7ae92ad87f22ffa1455f39385
swathi21051997 <swathirajendran1997@gmail.com>
2017-06-13 10:48:30 -0700

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Hello
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i=0;i<5;i++)
+        {
+            System.out.println("hello");
+        }
+    }
+}
